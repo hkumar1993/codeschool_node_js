ee671583bcaa287f387610829d82dbba41553b06
Harsh <harsh@hkumar.me>
2018-01-19 18:40:22 -0800

    Add console logs to test asynchronous response

--- hello.js
+++ hello.js
@@ -2,12 +2,16 @@ var http = require('http'); // Require http module
 
 http.createServer( function(request, response){
     response.writeHead(200); // Code status code
-    response.write("Hello, this is dog."); // Response
-    response.write("Dog is running"); // Response
+    // response.write("Hello, this is dog."); // Response
+    response.write("Dog is running\n"); // Response
+    // console.log("Dog is running\n"); // Response
+    
     setTimeout(function(){
-        response.write("Dog is done.");
+        response.write("Dog is done.\n");
+        // console.log("Dog is done\n"); // Response
         response.end()
     }, 5000)
+
     // response.end(); // Close connection
 }).listen(8080); // Listen for connectino on 8080
 
