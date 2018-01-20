adb86c78dbcf9efec38dd6145a859a9870e49f94
Harsh <harsh@hkumar.me>
2018-01-19 18:25:06 -0800

    Simulate delay

--- hello.js
+++ hello.js
@@ -3,7 +3,12 @@ var http = require('http'); // Require http module
 http.createServer( function(request, response){
     response.writeHead(200); // Code status code
     response.write("Hello, this is dog."); // Response
-    response.end(); // Close connection
+    response.write("Dog is running"); // Response
+    setTimeout(function(){
+        response.write("Dog is done.");
+        response.end()
+    }, 5000)
+    // response.end(); // Close connection
 }).listen(8080); // Listen for connectino on 8080
 
 console.log('Listening on port 8080 ...');
